dc56ac1ee47a3a46b9fed8a910bb5c72a4c19225
meelayadiPC <meelayadi@outlook.com>
2018-05-15 23:41:59 +0200

    constructeur de la classe hero et film

--- src/MarvelFlow/MarvelFlow.Classes/Hero.cs
+++ src/MarvelFlow/MarvelFlow.Classes/Hero.cs
@@ -6,21 +6,113 @@ using System.Threading.Tasks;
 
 namespace MarvelFlow.Classes
 {
+    enum Team
+    {
+        Avengers,
+        GuardienGalaxy,
+        Dora,
+        BlackOrder,
+        FamilleAsgardian
+    }
+
+    enum Statut
+    {
+        Neutre,
+        Gentil,
+        Méchant
+    }
+
+    enum Universe
+    {
+        MCU,
+        SerieNetflix
+    }
     public class Hero
     {
-        private string Hid { get; set; }
-        private string NomHero { get; set; }
+        
+        // ATTRIBUT //
 
-        private string NomReel { get; set; }
+        private string hId { get; set; }
 
-        private string Desc { get; set; }
+        private string nomHero { get; set; }
 
-        private string Pouvoir { get; set; }
+        private string image { get; set; }
 
-        private List<Movie> ListMovies { get;}
+        private string desc { get; set; }
 
-        private DateTime DateCrea { get; set; }
+        private Statut status { get; set; }
 
-        private float Popularité { get; set; }
+        private Boolean fav { get; set; }
+
+        private Team teamHeros;
+
+        private Universe univers { get; set; }
+
+        private List<Movie> listMovies { get;}
+
+        // CONSTRUCTEURS //
+        private Hero(string hId, string nomHero, string image, string desc, Statut status, Team teamHeros, Universe univers)
+        {
+            if (string.IsNullOrWhiteSpace(nomHero))
+            {
+                throw new ArgumentException("message", nameof(nomHero));
+            }
+
+            this.hId = hId;
+            this.nomHero = nomHero;
+            this.image = image;
+            this.desc = desc;
+            this.status = status;
+            fav = false;
+            this.teamHeros = teamHeros;
+            this.univers = univers;
+        }
+        
+        //constructeur avec certains attributs prédéfinis :
+                //  status = neutre  &  univers = MCU
+        private Hero(string hId, string image, string nomHero, string desc, Team teamHeros)
+        {
+            if (string.IsNullOrWhiteSpace(nomHero))
+            {
+                throw new ArgumentException("message", nameof(nomHero));
+            }
+
+            this.hId = hId;
+            this.nomHero = nomHero;
+            this.image = image;
+            this.desc = desc;
+            status = Statut.Neutre;
+            fav = false;
+            this.teamHeros = teamHeros;
+            univers = Universe.MCU;
+        }
+
+        // METHODES //
+        private Team setTeamHeros()
+                {
+                    return teamHeros;
+                }
+
+                private void setTeamHeros(Team value)
+                {
+                    teamHeros = value;
+                }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+       
     }
 }
--- src/MarvelFlow/MarvelFlow.Classes/Movie.cs
+++ src/MarvelFlow/MarvelFlow.Classes/Movie.cs
@@ -3,12 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace MarvelFlow.Classes
 {
-    public class Movie : Video
+    public abstract class Movie
     {
+        private string mId { get; set; }
+
+        private string titre { get; set; }
+
+        private string affiche { get; set; }
+
+        private string real { get; set; }
+
+        private bool sortie { get; }
+
+        private DateTime dateSortie { get; set; }
+
+        private readonly List<Hero> listHeros1;
+
         
-        private float dureeFilm { get; set; }
+        protected Movie(string mId, string titre, string affiche, string real, DateTime dateSortie, List<Hero> listHeros)
+        {
+            this.mId = mId;
+            this.titre = titre;
+            this.affiche = affiche;
+            this.real = real;
+            this.sortie = true;
+            this.dateSortie = dateSortie;
+            bool testSortie = dateSortie < DateTime.Now;
+            if (testSortie)
+            {
+                this.sortie = false ;
+            }
+            this.listHeros1 = listHeros;
+        }
+
+        private List<Hero> GetlistHeros()
+        {
+            return listHeros1;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
